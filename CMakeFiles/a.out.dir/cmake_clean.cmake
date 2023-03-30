e035ec8d9e683510b2b208f232dcaa4ddb8c3b1c
yulimmm <dbfla2ee@gmail.com>
2023-03-30 15:09:31 +0900

    first_linux_test

---
+++ CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/a.c.o"
+  "CMakeFiles/a.out.dir/b.c.o"
+  "a.out.pdb"
+  "a.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
