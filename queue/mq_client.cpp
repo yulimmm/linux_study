51814bde0872f2945a2fd9de2fbe936c0892df52
yulimmm <89307538+yulimmm@users.noreply.github.com>
2023-09-22 11:36:27 +0900

    Update and rename mq_client.c to mq_client.cpp

---
+++ queue/mq_client.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <fcntl.h>
+#include <mqueue.h>
+#include <cstdio>
+#include <cstring>
+
+int main(int argc, char **argv) {
+    struct mq_attr attr;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 128;
+    mqd_t mq;
+    char buf[128] = {0};
+
+    mq = mq_open("/reakwon_mq", O_WRONLY, 0666, &attr);
+    if (mq == (mqd_t)-1) {
+        perror("open error");
+        exit(0);
+    }
+
+    std::cin >> buf;  // C++ 스타일로 입력 받음
+
+    if (mq_send(mq, buf, strlen(buf), 1) == -1) {
+        perror("mq_send error");
+        exit(-1);
+    }
+
+    mq_close(mq);
+
+    return 0;
+}
